dbd28fa2f5077269edf779cc242a55247aef4fe6
Cezar Augusto Crummenauer <cezar.augusto.web@gmail.com>
2019-02-05 19:23:52 -0200

    Criando controller para tweets

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const routes = express.Router();
 
+const TweetController = require('./controllers/TweetController');
+
 routes.get('/', (req, res) => {
     return res.send('Hello World');
 });
